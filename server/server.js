9acae105e7a06199af66fabc9853eb17da048fea
subraAntoine <antoine.subra1@gmail.com>
2023-07-29 15:41:54 +0200

    server initialisation and db connection

--- server/server.js
+++ server/server.js
@@ -1,1 +1,42 @@
 //server
+require('dotenv').config();
+
+
+//import
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+//connect to mongoDB
+const mongoURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@pacepartner.boyyazb.mongodb.net/`;
+
+try {
+    mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+        .then(() => console.log("MongoDB connected"))
+        .catch(error => console.log("Error connecting to MongoDB:", error));
+} catch (error) {
+    console.log(error);
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
+//
