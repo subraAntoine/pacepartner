248d7c0f468bbec72293f64c257cd322cd5fbcfa
subraAntoine <antoine.subra1@gmail.com>
2023-11-17 18:30:02 +0800

    update

--- client/pacepartner/src/Api/User/Login.js
+++ client/pacepartner/src/Api/User/Login.js
@@ -1,24 +1,24 @@
-import axios from 'axios';
-import config from '../../config';
+import axios from "axios";
+import config from "../../config";
 const handleLogin = async (e, email, password) => {
-    e.preventDefault();
-    try {
+  e.preventDefault();
+  try {
+    const response = await axios.post(
+      `${config.apiUrl}/users/login`,
+      {
+        email,
+        password,
+      },
+      {
+        withCredentials: true,
+        crossDomain: true,
+      }
+    );
 
-        const response = await axios.post(`${config.apiUrl}/users/login`, {
-            email,
-            password
-        },
-            {
-                withCredentials: true
-            });
-
-
-        return response.status === 200;
-
-    } catch (error) {
-        return false;
-    }
-
-}
+    return response.status === 200;
+  } catch (error) {
+    return false;
+  }
+};
 
 export default handleLogin;
